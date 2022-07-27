92209f8bbf7f71647244c9b3455f1760e519dacc
hbadran83 <hbadran@uplandsoftware.com>
2022-07-27 17:46:21 +0200

    project submission 1

--- update.sh
+++ update.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1 
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1  --profile "udacity"
